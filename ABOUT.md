83388874522651dcdd5b1a37eb24b001a828373f
MalikHw <help.malicorporation@gmail.com>
2026-07-03 14:03:50 +0100

    yeea

---
+++ ABOUT.md
@@ -0,0 +1,7 @@
+## Drag-n-drop GMD
+if you ever wanted to drag a gmd file to the GD window to import it, now you can!
+
+### Credits:
+- [MaliKHw47](https://www.youtube.com/channel/UCyigqF_mR6nM2YnBOfKRfhQ/): code
+- [MalikHw47](https://www.youtube.com/channel/UCyigqF_mR6nM2YnBOfKRfhQ/): help
+- [MalikHw47](https://www.youtube.com/channel/UCyigqF_mR6nM2YnBOfKRfhQ/)'s gf: logo
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,17 @@
 #include <Geode/Geode.hpp>
+#include <hjfod.gmd-api/include/GMD.hpp>
+#include <camila314.drag-drop/include/DragDrop.hpp>
+#include <miskaa.notif/src/includes/notif.hpp>
+
+using namespace geode::prelude;
+
+$on_mod(Loaded) {
+    handleDragDrop({ "gmd" }, [](auto path) {
+        auto level = gmd::importGmdAsLevel(path);
+        if (level) {
+            notifapi::success("Imported!");
+        } else {
+            notifapi::error("Failed!");
+        }
+    });
+}
